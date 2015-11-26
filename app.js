5c7187475083ed7549db310bf036dc1b9c994f3b
John Oliver Mendoza <johnoliver.mendoza@servicenow.com>
2015-11-26 11:45:13 -0800

    Adding more heroku configuration elements

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
+//var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -38,10 +38,6 @@ var users = require('./routes/users');
 
 var app = express();
 
-//Heroku Port
-app.set('port', (process.env.PORT || 5000));
-
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -53,13 +49,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-
-//Heroku Listen for port
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
-
 
 
 app.use('/', routes);
