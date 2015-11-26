d0967b872e60b0daaf0d3c47dc0761fefc0cf19e
John Oliver Mendoza <johnoliver.mendoza@servicenow.com>
2015-11-26 11:26:00 -0800

    Added heroku support

--- app.js
+++ app.js
@@ -38,6 +38,10 @@ var users = require('./routes/users');
 
 var app = express();
 
+//Heroku Port
+app.set('port', (process.env.PORT || 5000));
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -49,6 +53,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+//Heroku Listen for port
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
 
 
 
